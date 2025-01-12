21a31a9b59e8ce802f324d527ab30118e7dfbdb9
AyalaR754 <ayali73754@gmail.com>
2025-01-12 16:24:27 +0200

    Update title.js

--- Server/routes/title.js
+++ Server/routes/title.js
@@ -5,20 +5,15 @@ const titleController=require("../Controllers/titleController")
 const verifyJWT=require("../middleware/verifyJWT")
 
 
-<<<<<<< HEAD
+
 
 router.post('/',verifyJWT,titleController.createNewTitle)
 
-=======
->>>>>>> 8e52b0a7f7d8b7dde7722859f9e84407c7b46943
 router.get('/getAllTitles/:id',titleController.getAllTitles)
 
 router.get('/getTitleById/:id',titleController.getTitleById)
 
-<<<<<<< HEAD
 router.delete('/:id',verifyJWT,titleController.deleteTitle)
-=======
-router.delete('/:id',titleController.deleteTitle)
->>>>>>> 8e52b0a7f7d8b7dde7722859f9e84407c7b46943
+
 
 module.exports=router
