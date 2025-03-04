9e930845f900a4d8f8e6de495289a90ab9c54915
תהילה גלבוע <328227616@mbydomain>
2025-03-04 21:14:15 +0200

    grads pictuers

--- Client/src/Components/Grade.jsx
+++ Client/src/Components/Grade.jsx
@@ -1,80 +1,3 @@
-// // import { useState } from "react";
-// import { Card } from 'primereact/card';
-// import { Button } from 'primereact/button';
-// import { Dialog } from 'primereact/dialog';
-// import { useState } from "react";
-
-// import '../Grade.css'; 
-
-// import UpdateGrade from "./UpdateGrade";
-// import 'primeicons/primeicons.css';
-// import axios from 'axios';
-
-// const Grade = (props) => {
-//     const [visible, setVisible] = useState(false);
-
-//     //**********updateGrade
-//     const updateGrade = async (nameRef, imageRef) => {
-//         const updatedGrade = {
-//             ...props.grade,
-//             name: nameRef.current.value ? nameRef.current.value : props.grade.title,
-//             image: imageRef.current.value ? imageRef.current.value : props.grade.body,
-//         };
-
-//         try {
-//             const res = await axios.put('http://localhost:7000/api/grade', updatedGrade)
-//             if (res.status === 200) {
-//                 console.log("res.data", res.data);
-//                 props.setGradesData(res.data);
-//             }
-//         } catch (e) {
-//             console.error(e)
-//         }
-//     }
-
-//     //************delete
-//     const deleteGrade = async (id) => {
-//         const res = await axios.delete(`http://localhost:7000/api/grade/${id}`)
-//         props.setGradesData(res.data)
-//     }
-
-//     //***********return
-//     const footer = (
-//         <div className="card flex flex-wrap gap-2 justify-content-center">
-//             <Button icon="pi pi-times" label="Delete" onClick={() => { deleteGrade(props.grade._id) }} />
-//             <Button label="Update" icon="pi pi-pencil" onClick={() => setVisible(true)} />
-//             <UpdateGrade updateGrade={updateGrade} setVisible={setVisible} visible={visible} grade={props.grade} />
-//         </div>
-//     );
-
-//     // const header = (
-//     //     <img alt="Card" src="/pictures/1.png" />
-//     // );
-
-//     return (
-//         <div className="card-container">
-//             <div className="card flex flex-wrap gap-2 justify-content-center">
-//                 <div className="card-item">
-//                     <Card 
-//                         title={props.grade.name} 
-//                         footer={footer} 
-//                         // header={header} 
-//                         className="grade-card md:w-25rem"
-//                         style={{ backgroundImage: `url(${props.grade.image || '/pictures/default.jpg'})`, backgroundSize: 'cover' }}
-//                     >
-//                         <p className="m-0">
-//                             {/* {props.grade.image} */}
-//                         </p>
-//                     </Card>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Grade;
-
-
 import { useState } from "react"
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
@@ -84,79 +7,66 @@ import UpdateGrade from "./UpdateGrade"
 
 import 'primeicons/primeicons.css';
 import axios from 'axios'
-    
+
 const Grade = (props) => {
     const [visible, setVisible] = useState(false);
 
-    
+
     //**********updateGrade
-    const updateGrade = async (nameRef,imageRef) => {
+    const updateGrade = async (nameRef, imageRef) => {
         const updatedGrade = {
-                        ...props.grade,
-                        name: nameRef.current.value ? nameRef.current.value : props.grade.title,
-                        image: imageRef.current.value ? imageRef.current.value : props.grade.body,
-                    };
-    try {
-        const res = await axios.put('http://localhost:7000/api/grade', updatedGrade)
-        if (res.status === 200) {
-           
-            console.log("res.data",res.data);
-            props.setGradesData(res.data)
+            ...props.grade,
+            name: nameRef.current.value ? nameRef.current.value : props.grade.title,
+            image: imageRef.current.value ? imageRef.current.value : props.grade.body,
+        };
+        try {
+            const res = await axios.put('http://localhost:7000/api/grade', updatedGrade)
+            if (res.status === 200) {
+
+                console.log("res.data", res.data);
+                props.setGradesData(res.data)
+            }
+        } catch (e) {
+            console.error(e)
         }
-    } catch (e) {
-        console.error(e)
     }
-}
-    
+
     //************delete
     const deleteGrade = async (id) => {
         const res = await axios.delete(`http://localhost:7000/api/grade/${id}`)
         props.setGradesData(res.data)
-        }
-    
+    }
 
-    //***********return
 
     const footer = (
 
-        <div  className="card flex flex-wrap gap-2 justify-content-center">
+        <div className="card flex flex-wrap gap-2 justify-content-center">
 
             <Button icon="pi pi-times" label="Delete" onClick={() => { deleteGrade(props.grade._id) }} />
 
             <Button label="Update" icon="pi pi-pencil" onClick={() => setVisible(true)} />
 
             {/* <GradesUpdate VisibleUpdatGrade={VisibleUpdatGrade} setVisibleUpdatGrade={setVisibleUpdatGrade} updateGrade={updateGrade} grade={props.grade} /> */}
-            <UpdateGrade updateGrade={updateGrade} setVisible={setVisible}  visible={visible} grade={props.grade}/>
-
-
+            <UpdateGrade updateGrade={updateGrade} setVisible={setVisible} visible={visible} grade={props.grade} />
         </div>
-
-
-
     );
-    const header = (
-        <img alt="Card" src="/pictures/1.png" />
-    );
+
     return (
-        <>
-            <div className="card flex flex-wrap gap-2 justify-content-center">
 
-                <div className="card">
-                    <Card backgroundimage={"./pictures/1.png"} title={props.grade.name} footer={footer} header={header}  className="md:w-25rem" >
-                    
-
-                        <p className="m-0">
-                            {/* {props.grade.image} */}
-                        </p>
-                    </Card>
+        <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2" key={props.grade._id}>
+            <div className="p-4 border-1 surface-border surface-card border-round">
+                <div className="flex flex-column align-items-center gap-3 py-5">
+                    <img className="w-9 shadow-2 border-round" src={`/pictures/${props.grade.name}.png `} alt={props.grade.name} footer={footer} />
+                    {/* ${grade.image} */}
+                    <div className="text-2xl font-bold">{props.grade.name} {footer}</div>
                 </div>
             </div>
-        </>
+        </div>
 
     )
 
 }
-    
+
 
 export default Grade
 
--- Client/src/Components/Grades.jsx
+++ Client/src/Components/Grades.jsx
@@ -5,21 +5,14 @@ import 'primeicons/primeicons.css';
 import axios from 'axios'
 import Grade from "./Grade"
 import CreatGrade from "./CreatGrade"
-// **********
-
 import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
-// import { Rating } from 'primereact/rating';
-// import { Tag } from 'primereact/tag';
-// import { classNames } from 'primereact/utils';
 
 const Grades = () => {
     const [gradesData, setGradesData] = useState([])
     const [visibleCreatGrade, setVisibleCreatGrade] = useState(false);
 
     //GET - getAllGrades/
-
     const getGrades = async () => {
-
         try {
             const res = await axios.get('http://localhost:7000/api/grade')
             if (res.status === 200) {
@@ -45,12 +38,10 @@ const Grades = () => {
 
     //******Grade - createGrade***** */
     const createGrade = async (nameRef, imageRef) => {
-
         const newGrade = {
             name: nameRef.current.value ? nameRef.current.value : " ",
             image: imageRef.current.value ? imageRef.current.value : " ",
         }
-
         try {
             const res = await axios.Grade('http://localhost:7000/api/grade', newGrade)
             if (res.status === 200) {
@@ -70,46 +61,29 @@ const Grades = () => {
         getGrades()
     }, [])
 
-    const gridItem = (grade) => {
-        return (
-            <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2" key={grade._id}>
-                <div className="p-4 border-1 surface-border surface-card border-round">
-                    <div className="flex flex-column align-items-center gap-3 py-5">
-                        <img className="w-9 shadow-2 border-round" src={`/pictures/1.png `} alt={grade.name} />
-                        {/* ${grade.image} */}
-                        <div className="text-2xl font-bold">{grade.name}</div>
-                    </div>
-                </div>
-            </div>
-        );
-    };
+
 
     const itemTemplate = (grade, index) => {
         if (!grade) {
             return;
         }
-        return gridItem(grade);
+        return <Grade grade={grade} getGrades={getGrades} setGradesData={setGradesData} />;
     };
 
 
     const listTemplate = (gradesData) => {
         return <div className="grid grid-nogutter">{gradesData.map((grade, index) => itemTemplate(grade, index))}</div>;
     };
+
+
     return (<>
 
         {/* if(maneger) */}
-        <Button icon="pi pi-plus" aria-label="Filter" onClick={() => setVisibleCreatGrade(true)} />
+        <Button icon="pi pi-plus" rounded aria-label="Filter" onClick={() => setVisibleCreatGrade(true)} />
         <CreatGrade createGrade={createGrade} setVisibleCreatGrade={setVisibleCreatGrade} visibleCreatGrade={visibleCreatGrade} />
 
-
-        {/* {
-            gradesData ? 
-                gradesData.sort((
-                Grade1,Grade2)=>Grade1._id-Grade2._id).map((p) => { return <Grade  grade={p} getGrades={getGrades} setGradesData={setGradesData}/> }) : null
-        } */}
-
         <div className="card">
-            <DataView value={gradesData} listTemplate={listTemplate} />
+            <DataView value={gradesData} listTemplate={listTemplate}  />
         </div>
 
     </>)
